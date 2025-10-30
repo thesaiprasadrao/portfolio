609ff29e6d26139e3b249c6ebd7f6077b168f274
Saiprasad Rao <165925927+thesaiprasadrao@users.noreply.github.com>
2025-10-30 22:34:50 +0530

    Fix typo in AboutPage component

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,6 +2,7 @@ import { FaUserCircle } from "react-icons/fa";
 
 export default function AboutPage() {
   return (
+    <div className="background-black min-h-screen flex flex-col items-center justify-center px-40 sm:px-8">Hellow</div>
     <div className="background-black min-h-screen flex flex-col items-center justify-center px-40 sm:px-8">
       {/* Profile Image & Header */}
       <div className="flex flex-col items-center mb-6">
