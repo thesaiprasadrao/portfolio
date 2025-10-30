182027ef6243e63974e951a19d9b1304fe1b6fb6
Saiprasad Rao <saiprasadrao1234@gmail.com>
2025-10-30 21:18:51 +0530

    update cv

--- src/components/DownloadCVButton.tsx
+++ src/components/DownloadCVButton.tsx
@@ -4,7 +4,7 @@ import styles from "./DownloadCVButton.module.css";
 export default function DownloadCVButton() {
   return (
     <div className={styles.button}>
-      <Link href="https://drive.google.com/file/d/1A9IX37cgRgOou2Y3JGbGIZKTUB4_c3_7/view?usp=drive_link" className={styles.anchor}>
+      <Link href="https://drive.google.com/file/d/1v8PFgKUHtAGn0lV3b909zvH59qMU58bN/view?usp=sharing" className={styles.anchor}>
         <span className={styles.text}>Download CV</span>
       </Link>
     </div>
